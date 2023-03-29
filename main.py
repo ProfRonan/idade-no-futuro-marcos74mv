2a26c7289bdbd3e58ffdd33e69de6b6b76a2dc45
Marcos Vinicius <128155251+marcos74mv@users.noreply.github.com>
2023-03-29 13:48:55 +0000

    idade futura

---
+++ main.py
@@ -0,0 +1,8 @@
+anoatual = int(input("Digite o ano atual: "))
+idade = int(input("Digite sua idade atual: "))
+anofuturo = int(input("Digite outro ano maior que o atual: "))
+nome = (input("Digite seu nome: "))
+
+idadefutura = (anofuturo - anoatual) + idade
+
+print (str(nome) + ", no ano de " + str(anofuturo) + " você terá " + str(idadefutura) + " anos.")git
